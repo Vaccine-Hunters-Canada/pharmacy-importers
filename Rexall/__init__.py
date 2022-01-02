f0f4e07d2944fe14141a462d4742feb6d23bd3ca
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2022-01-02 17:18:15 -0500

    Name the parameters for ease of reading

--- Rexall/__init__.py
+++ Rexall/__init__.py
@@ -2,52 +2,52 @@ import os
 from medmeapp import MedMeAppInterface
 
 
+TENANT_ID = "f10d8ca0-ec52-4a28-aabd-9bd3388dab34"
+ENTERPRISE_NAME = "REXALL"
+SUBDOMAIN = "rexall"
+ORG_ID = os.environ.get("VHC_ORG_REXALL")
+VACCINES = [
+    {
+        "type": 4,
+        "appointment_type_name": "COVID-19 Vaccine (Dose 2 - Pfizer)",
+        "tags": set(["12+ Year Olds", "Pfizer", "2nd Dose"]),
+    },
+    {
+        "type": 4,
+        "appointment_type_name": "COVID-19 Vaccine (Dose 3 - Pfizer)",
+        "tags": set(["12+ Year Olds", "Pfizer", "3rd Dose"]),
+    },
+    {
+        "type": 4,
+        "appointment_type_name": "Pediatric (5-11) COVID-19 Vaccine (Dose 1)",
+        "tags": set(["5-11 Year Olds", "Pfizer", "1st Dose"]),
+    },
+    {
+        "type": 4,
+        "appointment_type_name": "Pediatric (5-11) COVID-19 Vaccine (Dose 2)",
+        "tags": set(["5-11 Year Olds", "Pfizer", "2nd Dose"]),
+    },
+    {
+        "type": 3,
+        "appointment_type_name": "COVID-19 Vaccine (Dose 2 - Moderna)",
+        "tags": set(["12+ Year Olds", "Moderna", "2nd Dose"]),
+    },
+    {
+        "type": 3,
+        "appointment_type_name": "COVID-19 Vaccine (Dose 3 - Moderna)",
+        "tags": set(["12+ Year Olds", "Moderna", "3rd Dose"]),
+    },
+    {
+        "type": 5,
+        "appointment_type_name": "COVID-19 Vaccine (Dose 2 - AstraZeneca/COVISHIELD)",
+        "tags": set(["12+ Year Olds", "AstraZeneca", "2nd Dose"]),
+    },
+    {
+        "type": 5,
+        "appointment_type_name": "COVID-19 Vaccine (Dose 3 - AstraZeneca)",
+        "tags": set(["12+ Year Olds", "AstraZeneca", "3rd Dose"]),
+    },
+]
+
 async def main():
-    await MedMeAppInterface(
-        "f10d8ca0-ec52-4a28-aabd-9bd3388dab34",
-        "REXALL",
-        "rexall",
-        os.environ.get("VHC_ORG_REXALL"),
-        [
-            {
-                "type": 4,
-                "appointment_type_name": "COVID-19 Vaccine (Dose 2 - Pfizer)",
-                "tags": set(["12+ Year Olds", "Pfizer", "2nd Dose"]),
-            },
-            {
-                "type": 4,
-                "appointment_type_name": "COVID-19 Vaccine (Dose 3 - Pfizer)",
-                "tags": set(["12+ Year Olds", "Pfizer", "3rd Dose"]),
-            },
-            {
-                "type": 4,
-                "appointment_type_name": "Pediatric (5-11) COVID-19 Vaccine (Dose 1)",
-                "tags": set(["5-11 Year Olds", "Pfizer", "1st Dose"]),
-            },
-            {
-                "type": 4,
-                "appointment_type_name": "Pediatric (5-11) COVID-19 Vaccine (Dose 2)",
-                "tags": set(["5-11 Year Olds", "Pfizer", "2nd Dose"]),
-            },
-            {
-                "type": 3,
-                "appointment_type_name": "COVID-19 Vaccine (Dose 2 - Moderna)",
-                "tags": set(["12+ Year Olds", "Moderna", "2nd Dose"]),
-            },
-            {
-                "type": 3,
-                "appointment_type_name": "COVID-19 Vaccine (Dose 3 - Moderna)",
-                "tags": set(["12+ Year Olds", "Moderna", "3rd Dose"]),
-            },
-            {
-                "type": 5,
-                "appointment_type_name": "COVID-19 Vaccine (Dose 2 - AstraZeneca/COVISHIELD)",
-                "tags": set(["12+ Year Olds", "AstraZeneca", "2nd Dose"]),
-            },
-            {
-                "type": 5,
-                "appointment_type_name": "COVID-19 Vaccine (Dose 3 - AstraZeneca)",
-                "tags": set(["12+ Year Olds", "AstraZeneca", "3rd Dose"]),
-            },
-        ]
-    ).update_availabilities()
+    await MedMeAppInterface(TENANT_ID, ENTERPRISE_NAME, SUBDOMAIN, ORG_ID, VACCINES).update_availabilities()
--- ShoppersDrugMart/__init__.py
+++ ShoppersDrugMart/__init__.py
@@ -1,53 +1,52 @@
 import os
 from medmeapp import MedMeAppInterface
 
+TENANT_ID = "edfbb1a3-aca2-4ee4-bbbb-9237237736c4"
+ENTERPRISE_NAME = "SDM"
+SUBDOMAIN = "shoppersdrugmart"
+ORG_ID = os.environ.get("VHC_ORG_SHOPPERS_DRUG_MART")
+VACCINES = [
+    {
+        "type": 4,
+        "appointment_type_name": "COVID-19 Vaccine (Pfizer Dose 1)",
+        "tags": set(["12+ Year Olds", "Pfizer", "1st Dose"]),
+    },
+    {
+        "type": 4,
+        "appointment_type_name": "COVID-19 Vaccine (Pfizer Dose 2)",
+        "tags": set(["12+ Year Olds", "Pfizer", "2nd Dose"]),
+    },
+    {
+        "type": 4,
+        "appointment_type_name": "COVID-19 Vaccine (Pfizer Dose 3 or Booster Dose)",
+        "tags": set(["12+ Year Olds", "Pfizer", "3rd Dose"]),
+    },
+    {
+        "type": 4,
+        "appointment_type_name": "COVID-19 Vaccine (Pfizer Pediatric Dose 1)",
+        "tags": set(["5-11 Year Olds", "Pfizer", "1st Dose"]),
+    },
+    {
+        "type": 4,
+        "appointment_type_name": "COVID-19 Vaccine (Pfizer Pediatric Dose 2)",
+        "tags": set(["5-11 Year Olds", "Pfizer", "2nd Dose"]),
+    },
+    {
+        "type": 3,
+        "appointment_type_name": "COVID-19 Vaccine (Moderna Dose 1)",
+        "tags": set(["12+ Year Olds", "Moderna", "1st Dose"]),
+    },
+    {
+        "type": 3,
+        "appointment_type_name": "COVID-19 Vaccine (Moderna Dose 2)",
+        "tags": set(["12+ Year Olds", "Moderna", "2nd Dose"]),
+    },
+    {
+        "type": 3,
+        "appointment_type_name": "COVID-19 Vaccine (Moderna Dose 3 or Booster Dose)",
+        "tags": set(["12+ Year Olds", "Moderna", "3rd Dose"]),
+    },
+]
 
 async def main():
-    await MedMeAppInterface(
-        "edfbb1a3-aca2-4ee4-bbbb-9237237736c4",
-        "SDM",
-        "shoppersdrugmart",
-        os.environ.get("VHC_ORG_SHOPPERS_DRUG_MART"),
-        [
-            {
-                "type": 4,
-                "appointment_type_name": "COVID-19 Vaccine (Pfizer Dose 1)",
-                "tags": set(["12+ Year Olds", "Pfizer", "1st Dose"]),
-            },
-            {
-                "type": 4,
-                "appointment_type_name": "COVID-19 Vaccine (Pfizer Dose 2)",
-                "tags": set(["12+ Year Olds", "Pfizer", "2nd Dose"]),
-            },
-            {
-                "type": 4,
-                "appointment_type_name": "COVID-19 Vaccine (Pfizer Dose 3 or Booster Dose)",
-                "tags": set(["12+ Year Olds", "Pfizer", "3rd Dose"]),
-            },
-            {
-                "type": 4,
-                "appointment_type_name": "COVID-19 Vaccine (Pfizer Pediatric Dose 1)",
-                "tags": set(["5-11 Year Olds", "Pfizer", "1st Dose"]),
-            },
-            {
-                "type": 4,
-                "appointment_type_name": "COVID-19 Vaccine (Pfizer Pediatric Dose 2)",
-                "tags": set(["5-11 Year Olds", "Pfizer", "2nd Dose"]),
-            },
-            {
-                "type": 3,
-                "appointment_type_name": "COVID-19 Vaccine (Moderna Dose 1)",
-                "tags": set(["12+ Year Olds", "Moderna", "1st Dose"]),
-            },
-            {
-                "type": 3,
-                "appointment_type_name": "COVID-19 Vaccine (Moderna Dose 2)",
-                "tags": set(["12+ Year Olds", "Moderna", "2nd Dose"]),
-            },
-            {
-                "type": 3,
-                "appointment_type_name": "COVID-19 Vaccine (Moderna Dose 3 or Booster Dose)",
-                "tags": set(["12+ Year Olds", "Moderna", "3rd Dose"]),
-            },
-        ]
-    ).update_availabilities()
+    await MedMeAppInterface(TENANT_ID, ENTERPRISE_NAME, SUBDOMAIN, ORG_ID, VACCINES).update_availabilities()
