aa1be11e092b055851377f7e8536ccd41f129343
Andrew MacKinnon <andrewpmk@gmail.com>
2022-01-02 13:42:22 -0500

    Calendly importer

--- Calendly/__init__.py
+++ Calendly/__init__.py
@@ -1,43 +1,39 @@
-#import os
-#import re
+import os
 import csv
 from os import name
-#import json
+import json
 import aiohttp
 import datetime
-#import logging
-import asyncio
-import sys
-#from vhc import VHC
+from vhc import VHC
 
-#import azure.functions as func
+import azure.functions as func
 
-async def main():
-#async def main(mytimer: func.TimerRequest, stateblob) -> str:
+async def main(mytimer: func.TimerRequest, stateblob) -> str:
     pharmacies_csv = open('Calendly/pharmacies.csv')
     pharmacies_locations = csv.DictReader(pharmacies_csv)
-
-    #print(pharmacies_locations)
 
     headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36'
     }
 
-    #start_date = datetime.datetime.now(pytz.timezone("America/New_York")) # Get current time in Eastern time
+    state = {}
+    newstate = {}
+    if stateblob:
+        state = json.load(stateblob)
+
     start_date = datetime.datetime.utcnow()
-    print(start_date)
     end_date = start_date + datetime.timedelta(days=60) # Use approximately 2 months
 
     start_date_formatted = start_date.strftime('%Y-%m-%d')
     end_date_formatted = end_date.strftime('%Y-%m-%d')
 
     async with aiohttp.ClientSession(headers=headers) as session:
-        #vhc = VHC(
-        #    base_url=os.environ.get('BASE_URL'),
-        #    api_key=os.environ.get('API_KEY'),
-        #    org_id=os.environ.get('VHC_ORG_CALENDLY'),
-        #    session=session
-        #)
+        vhc = VHC(
+            base_url=os.environ.get('BASE_URL'),
+            api_key=os.environ.get('API_KEY'),
+            org_id=os.environ.get('VHC_ORG_CALENDLY'),
+            session=session
+        )
 
         for location in pharmacies_locations:
             address = location['address']
@@ -54,8 +50,6 @@ async def main():
 
             if availabilities.status == 200:
                 body = await availabilities.json()
-
-                #print(body)
 
                 vaccine_type = 1 # Unknown vaccine type
                 availability = False
@@ -76,19 +70,16 @@ async def main():
                     'tags': ""
                 }
 
-                print(location_data)
-                print(availability)
+                await vhc.add_availability(
+                    num_available=1 if availability else 0,
+                    num_total=1 if availability else 0,
+                    vaccine_type=vaccine_type,
+                    location=location_data,
+                    external_key=location['id']
+                )
 
-                # await vhc.add_availability(
-                #     num_available=1 if availability else 0,
-                #     num_total=1 if availability else 0,
-                #     vaccine_type=vaccine_type,
-                #     location=location_data,
-                #     external_key=location['id']
-                # )
+                if availability:
+                    name = f'{location["name"]} - ({location_data["city"]}, {location_data["province"]})'
+                    newstate[location["id"]] = name
 
-if __name__ == '__main__':
-    if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
-    asyncio.run(main())
+        return json.dumps(newstate)
