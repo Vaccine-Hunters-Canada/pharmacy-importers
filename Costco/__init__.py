c461431fde9b7ac61041ad2a7ebd17dc9730e6e8
Eric <eric.herscovich@shopify.com>
2022-01-03 19:14:23 -0500

    Find bookable locations

--- Costco/__init__.py
+++ Costco/__init__.py
@@ -9,15 +9,24 @@ from urllib.request import Request, urlopen
 
 
 class CostcoLocation:
-    def __init__(self, name, address, city, province, postal, phone, teleHippoId, booking_url):
+    def __init__(self, name, address, city, province, postal, phone, telehippo_id, booking_url):
         self.name = "Costco " + name
         self.address = address
         self.city = city
         self.province = province
         self.postal = postal
         self.phone = phone
-        self.teleHippoId = teleHippoId
+        self.telehippo_id = telehippo_id
         self.booking_url = booking_url
+        self.retailer_id = -1
+        self.covid_services = []
+    
+    @staticmethod
+    def get_external_key(location):
+        if location.retailer_id != -1:
+            return f"costco-{location.retailer_id}"
+        else:
+            return ""
 
 HEADERS = {
     "Accept": "application/json",
@@ -33,8 +42,12 @@ HEADERS = {
 }
 
 
+def getSlots(retailer_id, covid_service_id):
+    return True
+
 async def main():
     async with aiohttp.ClientSession(headers=HEADERS) as session:
+        # Get all Costco locations
         costco_location_url = "https://www.costcopharmacy.ca/assets/json/app.clinics.json"
         costco_location_request = Request(costco_location_url, headers={'User-Agent': 'Mozilla/5.0'})
         costco_location_response = urlopen(costco_location_request).read().decode()
@@ -47,37 +60,82 @@ async def main():
             costco_location = CostcoLocation(location["name"], location["address"], location["city"], location["provinceCode"], location["postalCode"], location["phone"], location["teleHippoId"], booking_url)
             pharmacies.append(costco_location)
 
-        test_id = "w533"
+        for pharmacy in pharmacies:
+            pharmacy_id = pharmacy.telehippo_id
+            site_url = "https://apipharmacy.telehippo.com/api/c/{}/graphql".format(pharmacy_id)
 
-        site_url = "https://apipharmacy.telehippo.com/api/c/{}/graphql".format(test_id)
-        query = """
-            \"query {
-                cRetailerWithSetting (data:{slug:\\\"w533\\\"}) { 
-                    success,
-                    error,
-                    data { 
-                        retailer { 
-                            id,
-                            name,
-                            slug,
-                            startTime,
-                            endTime 
+            query = ("""
+                query {
+                    cRetailerWithSetting (data:{slug:\"%s\"}) { 
+                        success,
+                        error,
+                        data { 
+                            retailer { 
+                                id,
+                                name,
+                                slug,
+                                startTime,
+                                endTime 
+                            } 
+                        }
+                    }
+                }
+            """ % pharmacy_id)
+
+            response = await session.post(site_url, json={"query": query})
+            try:
+                body = await response.json()
+                pharmacy.retailer_id = body['data']['cRetailerWithSetting']['data']['retailer']['id']
+            except (json.decoder.JSONDecodeError, KeyError, IndexError):
+                logging.error(
+                    "Failed to fetch data"
+                )
+                return []
+
+        for pharmacy in pharmacies:
+            retailer_id = pharmacy.retailer_id
+            # retailer_id = 135
+
+            appointments_url = "https://apipharmacy.telehippo.com/api/c/{}/graphql".format(retailer_id)
+            query = ("""
+                query { 
+                    onlineBookableAppointmentTypes (data:{retailerId:%s}) { 
+                        success,
+                        error,
+                        data {  
+                            services { 
+                                id,
+                                name,
+                                duration,
+                                bookingMode,
+                                description,
+                                intakeFormType,
+                                mainPageMode 
+                            } 
                         } 
                     }
                 }
-            }\"
-        """
-        
-        print(query)
-        response = await session.post(site_url, json={"query": query})
-        try:
-            body = await response.json()
-            print(body)
-        except (json.decoder.JSONDecodeError, KeyError, IndexError):
-            logging.error(
-                "Failed to fetch data"
-            )
-            return []
+            """ % retailer_id)
+            
+            response = await session.post(appointments_url, json={"query": query})
+            try:
+                body = await response.json()
+                available_services = body['data']['onlineBookableAppointmentTypes']['data']['services']
+
+                for service in available_services:
+                    # This isn't super clear from the API, but I was able to derive the following:
+                    # bookingMode = 0 - Shows up on the site
+                    # bookingMode = 6 - Hidden from user-facing site
+                    # mainPageMode = 0 - Accepting Bookable Appointments
+                    # mainPageMode = 2 - Accepting Waitlist Signups
+                    if (service['bookingMode'] == 0 and service['mainPageMode'] == 0 and "covid" in service['name'].lower()):
+                        pharmacy.covid_services.append(service)
+
+            except (json.decoder.JSONDecodeError, KeyError, IndexError):
+                logging.error(
+                    "Failed to fetch data"
+                )
+                return []
 
 
 loop = asyncio.get_event_loop()
